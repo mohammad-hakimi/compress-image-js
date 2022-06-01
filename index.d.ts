9c68a9fa402e34b8c8f139f5aed490a9573f2c35
Mohammad Hakimi <hakimi.mohammad.n@gmail.com>
2022-06-01 09:53:10 +0430

    Version 1.0.2 release --- add types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 
-
-declare class Compress {
-    constructor(size: [number, number], quality: number, mime_type: string);
-    process(image: string): {blob: Blob, dataUrl: string};
+declare module 'compress-image-js' {
+    class Compress {
+        constructor(size: [number, number], quality: number, mime_type: string);
+        process(image: string): {blob: Blob, dataUrl: string};
+    }
 }
